d02042c2b7f016850be7748b4c89a9a799369f67
Tamas Szucs <tomfurrier@gmail.com>
2015-03-11 17:15:17 +0100

    Basic frank stuff

--- step.sh
+++ step.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Hello ${EXAMPLE_INPUT_NAME}!"
+cd $FRANK_TESTING_PROJECT_PATH
+gem install frank-cucumber -N
+frank setup
+frank build
+frank launch
+
 
 # return 0 as the exit code in case of success
 # return >0 if the step failed
