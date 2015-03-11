66ee55402b247101945d10d8916c9ae69d4aeb32
Tamas Szucs <tomfurrier@gmail.com>
2015-03-11 17:24:55 +0100

    test

--- step.sh
+++ step.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "HALLLO"
+echo $FRANK_TESTING_PROJECT_ROOT_DIR_PATH
+echo $FRANK_TESTING_PROJECT_PATH
+cd $FRANK_TESTING_PROJECT_ROOT_DIR_PATH
 cd $FRANK_TESTING_PROJECT_PATH
 gem install frank-cucumber -N
 frank setup
